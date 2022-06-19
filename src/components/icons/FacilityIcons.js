049fc5f9e7a7775f6ed214d36a7dfd428a0e16ff
ical10 <mochhusnir@gmail.com>
2022-06-19 16:38:09 +0700

    feat: setup svgr webpack for svg icon rendering

---
+++ src/components/icons/FacilityIcons.js
@@ -0,0 +1,49 @@
+import SvgIcon from '@mui/material/SvgIcon';
+
+import React from 'react';
+
+import IconConservancyFee from '../../../public/IconConservancyFee.svg';
+import IconDailyRoomService from '../../../public/IconDailyRoomService.svg';
+import IconElectricity from '../../../public/IconElectricity.svg';
+import IconFreeParking from '../../../public/IconFreeParking.svg';
+import IconInternet from '../../../public/IconInternet.svg';
+import IconPlayButton from '../../../public/IconPlayButton.svg';
+import IconPropertytax from '../../../public/IconPropertytax.svg';
+import IconSinkingFunds from '../../../public/IconSinkingFunds.svg';
+import IconWaterService from '../../../public/IconWaterService.svg';
+
+export const ConservancyFeeIcon = props => (
+  <SvgIcon component={IconConservancyFee} viewBox="0 0 40 40" {...props} />
+);
+
+export const DailyRoomServiceIcon = props => (
+  <SvgIcon component={IconDailyRoomService} viewBox="0 0 40 40" {...props} />
+);
+
+export const ElectricityIcon = props => (
+  <SvgIcon component={IconElectricity} viewBox="0 0 40 40" {...props} />
+);
+
+export const FreeParkingIcon = props => (
+  <SvgIcon component={IconFreeParking} viewBox="0 0 40 40" {...props} />
+);
+
+export const InternetIcon = props => (
+  <SvgIcon component={IconInternet} viewBox="0 0 40 40" {...props} />
+);
+
+export const PlayButtonIcon = props => (
+  <SvgIcon component={IconPlayButton} viewBox="0 0 40 40" {...props} />
+);
+
+export const PropertyTaxIcon = props => (
+  <SvgIcon component={IconPropertytax} viewBox="0 0 40 40" {...props} />
+);
+
+export const SinkingFundsIcon = props => (
+  <SvgIcon component={IconSinkingFunds} viewBox="0 0 40 40" {...props} />
+);
+
+export const WaterServiceIcon = props => (
+  <SvgIcon component={IconWaterService} viewBox="0 0 40 40" {...props} />
+);
