cdf6e13109ec5196987b5d228e9a2659e286232e
ical10 <mochhusnir@gmail.com>
2022-06-20 21:56:35 +0700

    feat: added token distribution and roadmap section

--- pages/projects/lima-beach/SecondSection.js
+++ pages/projects/lima-beach/SecondSection.js
@@ -26,25 +26,27 @@ const SecondSection = () => {
       <NinthSubSection />
       <TenthSubSection />
       <div className={styles.tokenDistributionContainer}>
-        <div className={styles.imageContainer}>
-          <Image
-            src={'/Roadmap.svg'}
-            width="100%"
-            height="100%"
-            layout="responsive"
-            objectFit="contain"
-          />
+        <div className={styles.tokenDistributionHeaderContainer}>
+          <h1 className={styles.titleBig}>Token Distribution.</h1>
+          <label className={styles.tokenDistributionLabel}>1,771 NFT Fragment</label>
+        </div>
+        <div style={{width: '1140px', height: '646px', position: 'relative'}}>
+          <Image src={'/Chart.svg'} layout="fill" />
         </div>
       </div>
       <div className={styles.roadmapContainer}>
+        <div className={styles.roadmapHeaderContainer}>
+          <h1 className={styles.titleBig}>Lima Beach Signature NFT Roadmap.</h1>
+          <label className={styles.description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vulputate nunc tortor proin
+            adipiscing lacus augue gravida et. Vulputate nunc tortor proin adipiscing lacus augue
+            gravida et.
+          </label>
+        </div>
         <div className={styles.imageContainer}>
-          <Image
-            src={'/Token_Distribution.svg'}
-            width="100%"
-            height="100%"
-            layout="responsive"
-            objectFit="contain"
-          />
+          <div style={{width: '100%', height: '1368px', position: 'relative'}}>
+            <Image src={'/Roadmap.svg'} layout="fill" />
+          </div>
         </div>
       </div>
     </div>
