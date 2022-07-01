ded4588349027dc6d73e5ccb3268f46b3c403eee
ical10 <mochhusnir@gmail.com>
2022-07-01 11:38:41 +0700

    feat: added more components with mobile styling

--- src/components/ContentComponent.js
+++ src/components/ContentComponent.js
@@ -1,33 +1,86 @@
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+
+import {useState} from 'react';
+
+import {MinusCirlce, AddCircle, TickCircle, InfoCircle} from 'iconsax-react';
 import SocialHandles from 'src/components/SocialHandles';
+import {mintDigilandNFT, connectWallet} from 'src/helpers/metamask-interact';
 import styles from 'styles/ContentComponent.module.css';
 
-const ContentComponent = ({
-  onIncrement,
-  onDecrement,
-  onChangeReferralCode,
-  referralCode,
-  onChangeQuantity,
-  quantity,
-  mintedQuantity,
-  maxQuantity,
-  walletAddress = '',
-  mintStatus,
-  onMintPressed,
-  onConnectWallet,
-}) => {
+const ContentComponent = () => {
+  //TODO: add selector here from state management to read walletAddress
+
+  const [referralCode, setReferralCode] = useState('');
+  const [quantity, setQuantity] = useState(0);
+  const [mintedQuantity, setMintedQuantity] = useState(0);
+
+  const [, setMintStatus] = useState('');
+
+  const [walletAddress, setWalletAddress] = useState('');
+
+  const [isUSDCAllowed, setIsUSDCAllowed] = useState(false);
+
+  //Constant state
+  const [maxQuantity] = useState(80);
+
+  const handleMintPressed = async () => {
+    if (quantity <= maxQuantity - mintedQuantity) {
+      //call metamask here to mint
+      console.log(`minted ${quantity} NFTs!`);
+      let tempMinted = mintedQuantity + quantity;
+      setMintedQuantity(tempMinted);
+
+      const {status} = await mintDigilandNFT(quantity);
+      setMintStatus(status);
+    }
+  };
+
+  const handleDecrement = () => {
+    if (quantity !== 0) {
+      let decreasedQuantity = quantity - 1;
+      setQuantity(decreasedQuantity);
+    }
+  };
+
+  const handleIncrement = () => {
+    let increasedQuantity = quantity + 1;
+    setQuantity(increasedQuantity);
+  };
+
+  const handleChangeQuantity = e => {
+    setQuantity(Number(e.target.value));
+  };
+
+  const handleChangeReferralCode = e => {
+    setReferralCode(e.target.value);
+  };
+
+  const handleConnectWallet = async () => {
+    const walletResponse = await connectWallet();
+
+    //TODO: set walletAddress to the state management instead
+    setWalletAddress(walletResponse.address);
+  };
+
+  const handleAllowUSDC = () => {
+    //TODO: perform USDCAllowance extrinsic using Metamask first
+    setIsUSDCAllowed(!isUSDCAllowed);
+  };
+
   return (
     <div id="content">
       <div className="flex flex-col content-center justify-center min-w-fit">
         <div className="flex flex-col items-center p-6 gap-[10px] text-center bg-[#edf4f7] rounded-t-lg">
           <img src="/Hexagon.svg" alt="NFT image illustration" className="w-45 h-45" />
-          <h3 className="font-bold text-xl">LIMA BEACH NFT</h3>
+          <h3 className="font-bold text-xl">5 DAYS LEFT!</h3>
           <div>
             <p className="font-bold">Mint Price</p>
             <p className="font-normal">2000 USDC</p>
           </div>
           <div>
             <p className="font-bold">Total Supply</p>
-            <p className="font-normal">250/250</p>
+            <p className="font-normal">1,771</p>
           </div>
           <div className="flex flex-row gap-2">
             <SocialHandles />
@@ -35,8 +88,8 @@ const ContentComponent = ({
         </div>
         <div className="bg-[#fff] rounded-b-lg p-6">
           <div className="flex flex-col gap-2 mb-8">
-            <h1 className="font-bold text-2xl">PUBLIC SALE STAGE 1</h1>
-            <h3 className="font-semibold text-xl">Physical Land Stage</h3>
+            <h1 className="font-bold text-2xl uppercase">physical land stage</h1>
+            <h3 className="font-semibold text-lg uppercase">lima beach signature nft</h3>
           </div>
 
           <div className="flex flex-col gap-4 mb-8">
@@ -48,29 +101,31 @@ const ContentComponent = ({
               placeholder="Input Referral Code (Optional)"
               alt="Input Referral Code (Optional)"
               value={referralCode}
-              onChange={onChangeReferralCode}
+              onChange={handleChangeReferralCode}
             />
 
-            <div className="flex flex-row items-center gap-5">
-              <div className="flex">
-                <button className={styles.incrementorButton} onClick={onDecrement}>
-                  <span style={{color: 'black'}}>−</span>
-                </button>
+            <div className={styles.container}>
+              <div className="flex py-1 px-2 rounded-lg border border-[#d0d5dd]">
+                <div>
+                  <IconButton onClick={handleDecrement}>
+                    <MinusCirlce size="20" color="#8f98aa" />
+                  </IconButton>
+                </div>
                 <input
                   type="number"
                   name="input-mint-quantity"
                   className={styles.inputQuantity}
                   value={quantity}
-                  onChange={onChangeQuantity}
+                  onChange={handleChangeQuantity}
                 ></input>
-                <button className={styles.incrementorButton} onClick={onIncrement}>
-                  <span style={{color: 'black'}}>+</span>
-                </button>
+                <div>
+                  <IconButton onClick={handleIncrement}>
+                    <AddCircle size="20" color="#8f98aa" />
+                  </IconButton>
+                </div>
               </div>
-              <div className="whitespace-nowrap">
-                <h4 className="font-bold text-base">
-                  Total <span className="font-normal text-sm">7,000 USDC</span>
-                </h4>
+              <div className="flex items-center rounded-lg border border-[#d0d5dd] py-2 px-4">
+                <h4 className="font-bold text-sm whitespace-nowrap">50,000 USDC</h4>
               </div>
             </div>
 
@@ -78,35 +133,37 @@ const ContentComponent = ({
               <input type="checkbox" id="tnc" name="tnc" value="Agreed" />
               <label htmlFor="tos">
                 I agree with{' '}
-                <a href="" style={{color: '#406aff'}}>
+                <a href="https://google.com" style={{color: '#406aff'}}>
                   terms and conditions
                 </a>
               </label>
             </div>
           </div>
 
-          {
-            //<div className={styles.walletStatus}>
-            //<label>Minted supply : {mintedQuantity}</label>
-            //</div>
-          }
-
-          <div>
+          <div className="flex flex-col gap-4">
             {walletAddress.length > 0 ? (
               <>
-                <button>
-                  <span>Allow Digilandbali to trade your USDC</span>
+                <button
+                  className={isUSDCAllowed ? 'outlinedActive' : 'outlined'}
+                  onClick={handleAllowUSDC}
+                >
+                  Allow to trade {(quantity * 50000).toLocaleString()} USDC
+                  {isUSDCAllowed ? (
+                    <TickCircle size="16" color="#76CE8A" />
+                  ) : (
+                    <InfoCircle size="16" color="#1F50FF" />
+                  )}
                 </button>
                 <button
                   className="w-full"
-                  onClick={onMintPressed}
+                  onClick={handleMintPressed}
                   disabled={quantity > 0 ? false : true}
                 >
                   <span>Mint Now</span>
-                </button>
+                </button>{' '}
               </>
             ) : (
-              <button className="w-full" onClick={onConnectWallet}>
+              <button className="w-full" onClick={handleConnectWallet}>
                 <span>Connect Wallet</span>
               </button>
             )}
--- src/components/MintComponent.js
+++ src/components/MintComponent.js
@@ -1,73 +1,10 @@
-import {useState} from 'react';
-
 import ContentComponent from './ContentComponent';
 
-import {mintDigilandNFT} from 'src/helpers/metamask-interact';
-
 const MintComponent = ({walletAddress}) => {
-  //State for forms
-  const [referralCode, setReferralCode] = useState('');
-  const [quantity, setQuantity] = useState(0);
-  const [mintedQuantity, setMintedQuantity] = useState(0);
-
-  const [mintStatus, setMintStatus] = useState('');
-
-  //Constant state
-  const [maxQuantity] = useState(80);
-
-  const handleMintPressed = async () => {
-    if (quantity <= maxQuantity - mintedQuantity) {
-      //call metamask here to mint
-      console.log(`minted ${quantity} NFTs!`);
-      let tempMinted = mintedQuantity + quantity;
-      setMintedQuantity(tempMinted);
-
-      const {status} = await mintDigilandNFT(quantity);
-      setMintStatus(status);
-    }
-  };
-
-  const handleDecrement = () => {
-    if (quantity !== 0) {
-      let decreasedQuantity = quantity - 1;
-      setQuantity(decreasedQuantity);
-    }
-  };
-
-  const handleIncrement = () => {
-    let increasedQuantity = quantity + 1;
-    setQuantity(increasedQuantity);
-  };
-
-  const handleChangeQuantity = e => {
-    setQuantity(Number(e.target.value));
-  };
-
-  const handleChangeReferralCode = e => {
-    setReferralCode(e.target.value);
-  };
-
-  const handleConnectWallet = () => {
-    console.log('connect wallet');
-  };
-
   return (
     <>
       <div>
-        <ContentComponent
-          onIncrement={handleIncrement}
-          onDecrement={handleDecrement}
-          onChangeReferralCode={handleChangeReferralCode}
-          referralCode={referralCode}
-          onChangeQuantity={handleChangeQuantity}
-          quantity={quantity}
-          mintedQuantity={mintedQuantity}
-          maxQuantity={maxQuantity}
-          walletAddress={walletAddress}
-          mintStatus={mintStatus}
-          onMintPressed={handleMintPressed}
-          onConnectWallet={handleConnectWallet}
-        />
+        <ContentComponent walletAddress={walletAddress} />
       </div>
     </>
   );
